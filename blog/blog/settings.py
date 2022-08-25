da4f3babc08199fddf44fe2b17fcbe715d5d58a0
dinh <congdinhpro2k@gmail.com>
2022-08-25 17:21:10 +0700

    hide secret

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -1,5 +1,9 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+dotenv_path = Path('./.env')
+load_dotenv(dotenv_path=dotenv_path)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!!9xkibq&bwfb&+la6!_0b&ng0y+hvbkhdd8h97bj25mey)xs9'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
