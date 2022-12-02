b2a17fb6b6d6ddee4530c3d453c2240fb7ade6e8
dinh <congdinhpro2k@gmail.com>
2022-12-02 15:43:25 +0700

    add product app

--- fb/settings.py
+++ fb/settings.py
@@ -28,7 +28,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg',
-    'address'
+    'address',
+    'product'
 ]
 
 MIDDLEWARE = [
--- fb/urls.py
+++ fb/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('user.api.api_urls')),
     path('api/', include('address.api.urls')),
+    path('api/', include('product.api.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
